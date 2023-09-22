484623aabd9752037d1ebafb6929b41a015a7bb2
JorgenKH04 <131149458+JorgenKH04@users.noreply.github.com>
2023-09-22 12:47:14 +0200

    refactor: :recycle: Naming
    Redo naming conventions for formatter

--- src/data/lang.no.ts
+++ src/data/lang.no.ts
@@ -1,39 +1,38 @@
-export class lang extends Array<string> {
-	greeting = "Hei, jeg heter Jørgen";
-	introparagraph1 =
-		"Jeg er en 20 år gammel webutvikler med en lidenskap for teknologi. Jeg har tatt et frontend-fokusert kurs som heter Kodehode de siste 6 månedene hvor jeg har fått en god innføring i HTML, CSS, Js/Ts, React, Figma. Jeg er veldig spent på å bruke de nye evnene mine på en praksisplass.";
-	introparagraph2 =
-		"Fra det øyeblikket jeg fikk tak i min første spillkonsoll, var det som om en helt ny verden åpnet seg. Mulighetene, gleden ved problemløsning og teknologiens evige utvikling har gitt meg en urokkelig lidenskap som forsterkes for hvert gjennombrudd.";
-	introparagraph3 =
-		"På fritiden liker jeg å spille videospill, spesielt taktiske skytespill sammen med venner. Men jeg er også fascinert av ny teknologi, for eksempel kunstig intelligens, og utforsker den på fritiden. Jeg er spesielt spent på fremtiden for kunstig intelligens og synes læringsmodellene deres er utrolig interessante. Jeg liker rett og slett bare tanken på å lære nye ting, og jeg er veldig spent på fremtiden min i teknologibransjen.";
-	contactbutton = "Kontakt meg";
-	contact = "Kontakt";
-	projects = "Prosjekter";
-	projectsbutton = "Mine prosjekter";
-	aboutme = "Om meg";
-	communicator = "God kommunikasjon";
-	flexible = "Fleksibel";
-	criticalthinker = "Kritisk tenker";
-	problemsolver = "Problemløser";
-	organized = "Organisert";
-	adaptable = "God tilpassningsevne";
-	myskills = "Mine ferdigheter";
-	collaborationheader = "Ønsker du å sammarbeide?";
-	collaborationpart1 =
-		"Jeg er alltid åpen for nye spennende muligheter og ser frem til å høre fra deg. Ta gjerne kontakt via e-post på";
-	collaborationemail = " jorgenkh04.kodehode@gmail.com ";
-	collaborationpart2 =
-		"eller fyll ut skjemaet nedenfor. Jeg svarer så raskt som mulig og ser frem til å diskutere hvordan vi kan skape noe fantastisk sammen!";
-	ctacontact = "Kontakt meg";
-	moreprojects = "Se mer";
-	misc = "Diverse";
-	attribution = "Attribusjon";
-	icons8 = "Icons8";
-	animation = "Animasjon";
-	getintouch = "Kom i kontakt";
-	language = "NO";
-	formname = "Navnet ditt";
-	formemail = "Eposten din";
-	formmessage = "Send meg en melding.";
-	formsubmit = "Send";
+export class Lang extends Array<string> {
+  greeting = 'Hei, jeg heter Jørgen';
+  introparagraph1 =
+    'Jeg er en 20 år gammel webutvikler med en lidenskap for teknologi. Jeg har tatt et frontend-fokusert kurs som heter Kodehode de siste 6 månedene hvor jeg har fått en god innføring i HTML, CSS, Js/Ts, React, Figma. Jeg er veldig spent på å bruke de nye evnene mine på en praksisplass.';
+  introparagraph2 =
+    'Fra det øyeblikket jeg fikk tak i min første spillkonsoll, var det som om en helt ny verden åpnet seg. Mulighetene, gleden ved problemløsning og teknologiens evige utvikling har gitt meg en urokkelig lidenskap som forsterkes for hvert gjennombrudd.';
+  introparagraph3 =
+    'På fritiden liker jeg å spille videospill, spesielt taktiske skytespill sammen med venner. Men jeg er også fascinert av ny teknologi, for eksempel kunstig intelligens, og utforsker den på fritiden. Jeg er spesielt spent på fremtiden for kunstig intelligens og synes læringsmodellene deres er utrolig interessante. Jeg liker rett og slett bare tanken på å lære nye ting, og jeg er veldig spent på fremtiden min i teknologibransjen.';
+  contactbutton = 'Kontakt meg';
+  contact = 'Kontakt';
+  projects = 'Prosjekter';
+  projectsbutton = 'Mine prosjekter';
+  aboutme = 'Om meg';
+  communicator = 'God kommunikasjon';
+  flexible = 'Fleksibel';
+  criticalthinker = 'Kritisk tenker';
+  problemsolver = 'Problemløser';
+  organized = 'Organisert';
+  adaptable = 'God tilpassningsevne';
+  myskills = 'Mine ferdigheter';
+  collaborationheader = 'Ønsker du å sammarbeide?';
+  collaborationpart1 = 'Jeg er alltid åpen for nye spennende muligheter og ser frem til å høre fra deg. Ta gjerne kontakt via e-post på';
+  collaborationemail = ' jorgenkh04.kodehode@gmail.com ';
+  collaborationpart2 =
+    'eller fyll ut skjemaet nedenfor. Jeg svarer så raskt som mulig og ser frem til å diskutere hvordan vi kan skape noe fantastisk sammen!';
+  ctacontact = 'Kontakt meg';
+  moreprojects = 'Se mer';
+  misc = 'Diverse';
+  attribution = 'Attribusjon';
+  icons8 = 'Icons8';
+  animation = 'Animasjon';
+  getintouch = 'Kom i kontakt';
+  language = 'NO';
+  formname = 'Navnet ditt';
+  formemail = 'Eposten din';
+  formmessage = 'Send meg en melding.';
+  formsubmit = 'Send';
 }
--- src/data/language.ts
+++ src/data/language.ts
@@ -1,51 +1,51 @@
-import { lang as lang_nb_NO } from "./lang.no.ts";
-import { lang as lang_en_US } from "./lang.en.ts";
+import { Lang as LANG_NB_NO } from './lang.no';
+import { Lang as LANG_EN_US } from './lang.en';
 
 export interface LanguageType extends Array<string> {
-	greeting: string;
-	introparagraph1: string;
-	introparagraph2: string;
-	introparagraph3: string;
-	contactbutton: string;
-	contact: string;
-	projectsbutton: string;
-	projects: string;
-	aboutme: string;
-	communicator: string;
-	flexible: string;
-	criticalthinker: string;
-	problemsolver: string;
-	organized: string;
-	adaptable: string;
-	myskills: string;
-	collaborationheader: string;
-	collaborationpart1: string;
-	collaborationemail: string;
-	collaborationpart2: string;
-	ctacontact: string;
-	moreprojects: string;
-	misc: string;
-	attribution: string;
-	icons8: string;
-	animation: string;
-	getintouch: string;
-	language: string;
-	formname: string;
-	formemail: string;
-	formmessage: string;
-	formsubmit: string;
+  greeting: string;
+  introparagraph1: string;
+  introparagraph2: string;
+  introparagraph3: string;
+  contactbutton: string;
+  contact: string;
+  projectsbutton: string;
+  projects: string;
+  aboutme: string;
+  communicator: string;
+  flexible: string;
+  criticalthinker: string;
+  problemsolver: string;
+  organized: string;
+  adaptable: string;
+  myskills: string;
+  collaborationheader: string;
+  collaborationpart1: string;
+  collaborationemail: string;
+  collaborationpart2: string;
+  ctacontact: string;
+  moreprojects: string;
+  misc: string;
+  attribution: string;
+  icons8: string;
+  animation: string;
+  getintouch: string;
+  language: string;
+  formname: string;
+  formemail: string;
+  formmessage: string;
+  formsubmit: string;
 }
 
 export class Language {
-	language: string;
-	lang: LanguageType = new lang_en_US();
+  language: string;
+  lang: LanguageType = new LANG_EN_US();
 
-	constructor(language: string) {
-		this.language = language;
-		if (language === "nb-NO") {
-			this.lang = new lang_nb_NO();
-		} else if (language === "en-US") {
-			this.lang = new lang_en_US();
-		}
-	}
+  constructor(language: string) {
+    this.language = language;
+    if (language === 'nb-NO') {
+      this.lang = new LANG_NB_NO();
+    } else if (language === 'en-US') {
+      this.lang = new LANG_EN_US();
+    }
+  }
 }
