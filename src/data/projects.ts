6782d7e2b4e74a9b92a51db74101f8009c644a56
JorgenKH04 <131149458+JorgenKH04@users.noreply.github.com>
2023-09-22 12:10:54 +0200

    refactor: :recycle: Imports
    Changed imports for standardization

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,66 +1,66 @@
-import * as weatherTrackr from "../assets/project-images/weatherTrackr.png";
-import * as cryptify from "../assets/project-images/cryptify.png";
-import * as taskbuddy from "../assets/project-images/taskbuddy.png";
-import * as portfolio from "../assets/project-images/portofolio.png";
-import * as temp from "../assets/project-images/temp.jpg";
+import * as weatherTrackr from '@assets/project-images/weatherTrackr.png';
+import * as cryptify from '@assets/project-images/cryptify.png';
+import * as taskbuddy from '@assets/project-images/taskbuddy.png';
+import * as portfolio from '@assets/project-images/portofolio.png';
+import * as temp from '@assets/project-images/temp.jpg';
 
 export const Projects = [
-	{
-		name: "Portofolio",
-		github: "https://github.com/JorgenKH04/Portofolio",
-		netlify: "https://jorgen-portofolio.netlify.app/",
-		shortdescription: `This portfolio project serves as a comprehensive showcase of your professional
+  {
+    name: 'Portofolio',
+    github: 'https://github.com/JorgenKH04/Portofolio',
+    netlify: 'https://jorgen-portofolio.netlify.app/',
+    shortdescription: `This portfolio project serves as a comprehensive showcase of your professional
              profile and achievements. It represents the site you are currently visiting, encompassing your
               skills, contact information, and a curated selection of notable projects.`,
-		img: portfolio.default,
-		role: "",
-	},
-	{
-		name: "Taskbuddy",
-		github: "https://github.com/JorgenKH04/TaskBuddy",
-		netlify: "",
-		shortdescription: `Discover a powerful task management app that allows you to effortlessly 
+    img: portfolio.default,
+    role: '',
+  },
+  {
+    name: 'Taskbuddy',
+    github: 'https://github.com/JorgenKH04/TaskBuddy',
+    netlify: '',
+    shortdescription: `Discover a powerful task management app that allows you to effortlessly 
 			create, track, and organize your tasks. Seamlessly save tasks to local storage, 
 			mark them as completed or incomplete, and conveniently sort them alphabetically or by creation time. 
 			Stay productive and organized with this intuitive task management solution.`,
-		img: taskbuddy.default,
-		role: "",
-	},
-	{
-		name: "Cryptify",
-		github: "https://github.com/rondDev/cryptify/tree/master",
-		netlify: "",
-		shortdescription: `An all-in-one web app for base64 encoding, hex-text conversion, 
+    img: taskbuddy.default,
+    role: '',
+  },
+  {
+    name: 'Cryptify',
+    github: 'https://github.com/rondDev/cryptify/tree/master',
+    netlify: '',
+    shortdescription: `An all-in-one web app for base64 encoding, hex-text conversion, 
 			MD5/SHA1 hashing, URL encoding/decoding, JSON web tokens, 
 			case modification, and whitespace removal.`,
-		img: cryptify.default,
-		role: "",
-	},
-	{
-		name: "WeatherTrackr",
-		github: "https://github.com/JorgenKH04/WeatherTrackr",
-		netlify: "",
-		shortdescription: `WeatherTrackr utilizes an API to gather weather data for the next 
+    img: cryptify.default,
+    role: '',
+  },
+  {
+    name: 'WeatherTrackr',
+    github: 'https://github.com/JorgenKH04/WeatherTrackr',
+    netlify: '',
+    shortdescription: `WeatherTrackr utilizes an API to gather weather data for the next 
 			five days on one tab and the next five hours on another. This allows you to 
 			conveniently access and plan around detailed forecasts, 
 			helping you make informed decisions based on upcoming weather conditions.`,
-		img: weatherTrackr.default,
-		role: "",
-	},
-	{
-		name: "5",
-		github: "https://github.com/JorgenKH04/Portofolio",
-		netlify: "https://jorgen-portofolio.netlify.app/",
-		shortdescription: "Empty proj",
-		img: temp.default,
-		role: "",
-	},
-	{
-		name: "6",
-		github: "https://github.com/JorgenKH04/Portofolio",
-		netlify: "https://jorgen-portofolio.netlify.app/",
-		shortdescription: "Empty proj",
-		img: temp.default,
-		role: "",
-	},
+    img: weatherTrackr.default,
+    role: '',
+  },
+  {
+    name: '5',
+    github: 'https://github.com/JorgenKH04/Portofolio',
+    netlify: 'https://jorgen-portofolio.netlify.app/',
+    shortdescription: 'Empty proj',
+    img: temp.default,
+    role: '',
+  },
+  {
+    name: '6',
+    github: 'https://github.com/JorgenKH04/Portofolio',
+    netlify: 'https://jorgen-portofolio.netlify.app/',
+    shortdescription: 'Empty proj',
+    img: temp.default,
+    role: '',
+  },
 ];
--- src/data/skilldata.ts
+++ src/data/skilldata.ts
@@ -1,44 +1,44 @@
-import * as htmlLogo from "../assets/skill-icons/HTML5 Logo Badge-cropped.svg";
-import * as cssLogo from "../assets/skill-icons/icons8-css-cropped.svg";
-import * as jsLogo from "../assets/skill-icons/icons8-javascript-cropped.svg";
-import * as tsLogo from "../assets/skill-icons/icons8-typescript-cropped.svg";
-import * as nextLogoDark from "../assets/skill-icons/next-js-dark.svg";
-import * as nextLogoLight from "../assets/skill-icons/next-js-light.svg";
-import * as reactLogo from "../assets/skill-icons/icons8-react-cropped.svg";
-import * as csharpLogo from "../assets/skill-icons/icons8-c-sharp-logo-cropped.svg";
-import * as nodeLogo from "../assets/skill-icons/icons8-nodejs-cropped.svg";
-import * as mongodbLogo from "../assets/skill-icons/icons8-mongodb-cropped.svg";
-import * as dockerLogo from "../assets/skill-icons/icons8-docker-cropped.svg";
-import * as linuxLogo from "../assets/skill-icons/Tux-cropped.svg";
-import * as gitLogo from "../assets/skill-icons/icons8-git-cropped.svg";
-import * as figmaLogo from "../assets/skill-icons/icons8-figma-cropped.svg";
-import * as photoshopLogo from "../assets/skill-icons/icons8-photoshop-cropped.svg";
-import * as illustratorLogo from "../assets/skill-icons/icons8-illustrator-cropped.svg";
+import * as htmlLogo from '@assets/skill-icons/HTML5 Logo Badge-cropped.svg';
+import * as cssLogo from '@assets/skill-icons/icons8-css-cropped.svg';
+import * as jsLogo from '@assets/skill-icons/icons8-javascript-cropped.svg';
+import * as tsLogo from '@assets/skill-icons/icons8-typescript-cropped.svg';
+import * as nextLogoDark from '@assets/skill-icons/next-js-dark.svg';
+import * as nextLogoLight from '@assets/skill-icons/next-js-light.svg';
+import * as reactLogo from '@assets/skill-icons/icons8-react-cropped.svg';
+import * as csharpLogo from '@assets/skill-icons/icons8-c-sharp-logo-cropped.svg';
+import * as nodeLogo from '@assets/skill-icons/icons8-nodejs-cropped.svg';
+import * as mongodbLogo from '@assets/skill-icons/icons8-mongodb-cropped.svg';
+import * as dockerLogo from '@assets/skill-icons/icons8-docker-cropped.svg';
+import * as linuxLogo from '@assets/skill-icons/Tux-cropped.svg';
+import * as gitLogo from '@assets/skill-icons/icons8-git-cropped.svg';
+import * as figmaLogo from '@assets/skill-icons/icons8-figma-cropped.svg';
+import * as photoshopLogo from '@assets/skill-icons/icons8-photoshop-cropped.svg';
+import * as illustratorLogo from '@assets/skill-icons/icons8-illustrator-cropped.svg';
 
 export const frontEnd = [
-	{ img: htmlLogo.default, tech: "HTML" },
-	{ img: cssLogo.default, tech: "CSS" },
-	{ img: jsLogo.default, tech: "Javascript" },
-	{ img: tsLogo.default, tech: "Typescript" },
-	{
-		img: nextLogoLight.default,
-		imgLightMode: nextLogoDark.default,
-		tech: "Next.js",
-	},
-	{ img: reactLogo.default, tech: "React" },
+  { img: htmlLogo.default, tech: 'HTML' },
+  { img: cssLogo.default, tech: 'CSS' },
+  { img: jsLogo.default, tech: 'Javascript' },
+  { img: tsLogo.default, tech: 'Typescript' },
+  {
+    img: nextLogoLight.default,
+    imgLightMode: nextLogoDark.default,
+    tech: 'Next.js',
+  },
+  { img: reactLogo.default, tech: 'React' },
 ];
 
 export const backEnd = [
-	{ img: csharpLogo.default, tech: "C#" },
-	{ img: nodeLogo.default, tech: "Node.js" },
-	{ img: mongodbLogo.default, tech: "MongoDB" },
-	{ img: dockerLogo.default, tech: "Docker" },
-	{ img: linuxLogo.default, tech: "Linux" },
-	{ img: gitLogo.default, tech: "Git" },
+  { img: csharpLogo.default, tech: 'C#' },
+  { img: nodeLogo.default, tech: 'Node.js' },
+  { img: mongodbLogo.default, tech: 'MongoDB' },
+  { img: dockerLogo.default, tech: 'Docker' },
+  { img: linuxLogo.default, tech: 'Linux' },
+  { img: gitLogo.default, tech: 'Git' },
 ];
 
 export const design = [
-	{ img: figmaLogo.default, tech: "Figma" },
-	{ img: photoshopLogo.default, tech: "Photoshop" },
-	{ img: illustratorLogo.default, tech: "Illustrator" },
+  { img: figmaLogo.default, tech: 'Figma' },
+  { img: photoshopLogo.default, tech: 'Photoshop' },
+  { img: illustratorLogo.default, tech: 'Illustrator' },
 ];
