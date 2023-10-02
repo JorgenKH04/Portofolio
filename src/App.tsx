d59943588fb0102121a965c0844432cc2f7c0c58
JorgenKH04 <131149458+JorgenKH04@users.noreply.github.com>
2023-10-02 12:29:16 +0200

    fix: :bug: Component & navigation
    Added new component for use on unfinished pages, fixed bug where navigating to sites would not scroll to the top

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, ReactNode } from "react";
+import { useLocation } from "react-router-dom";
 
 import { LayoutBase } from "@layout/LayoutBase";
 import { Homepage } from "@components/Pages/Homepage";
-import { ProjectPage } from "@components/Pages/ProjectPage";
-import { Attribution } from "@components/Pages/Attribution";
+// import { ProjectPage } from "@components/Pages/ProjectPage";
+// import { Attribution } from "@components/Pages/Attribution";
+import { InDevelopment } from "@components/common/InDevelopment";
 import { LanguageProvider } from "@contexts/languageContext";
 import { LayoutProvider } from "@contexts/layoutContext";
 import { ThemeProvider } from "@contexts/themeContext";
@@ -14,18 +17,29 @@ function App() {
       <LayoutProvider>
         <ThemeProvider>
           <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<LayoutBase />}>
-                <Route index={true} element={<Homepage />} />
-                <Route path="projects" element={<ProjectPage />} />
-                <Route path="attribution" element={<Attribution />} />
-              </Route>
-            </Routes>
+            <ScrollToTop>
+              <Routes>
+                <Route path="/" element={<LayoutBase />}>
+                  <Route index={true} element={<Homepage />} />
+                  <Route path="projects" element={<InDevelopment />} />
+                  <Route path="attribution" element={<InDevelopment />} />
+                  <Route path="projects/:project" element={<InDevelopment />} />
+                </Route>
+              </Routes>
+            </ScrollToTop>
           </BrowserRouter>
         </ThemeProvider>
       </LayoutProvider>
     </LanguageProvider>
   );
+}
+
+function ScrollToTop({ children }: { children: ReactNode }) {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+  return <>{children}</>;
 }
 
 export default App;
